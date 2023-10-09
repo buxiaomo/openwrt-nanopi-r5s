69039e32eb1e4e5789c19baa8db50b19e0642452
GitHub Action <action@github.com>
2023-10-09 13:21:30 +0800

    add faq

--- action.sh
+++ action.sh
@@ -29,7 +29,7 @@ function init() {
 		sudo apt-get clean all
 	)
 	sudo apt-get update
-	sudo apt-get dist-upgrade -y
+	# sudo apt-get dist-upgrade -y
 	sudo apt-get -y install ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 		 bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
 		 git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
